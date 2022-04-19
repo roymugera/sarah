eafffc259d2556c7d98169a40478ea057f3ff441
Roy Mugera <roymugera@Macs-MacBook-Air.local>
2022-04-19 09:30:52 +0100

    First Commit

---
+++ sarah.js
@@ -0,0 +1,12 @@
+function menuOpen () {
+  document.getElementById("side_menu").style.width = '240px';
+}
+
+function menuClose () {
+  document.getElementById("side_menu").style.width = '0';
+};
+
+function sideView () {
+  document.getElementById("side_menu").style.width = '0';
+}
+
